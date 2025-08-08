62c8628e2f7c1300c1c4327ea8d4e4d73d66eaca
SeungYeop Oh <sadrain9274@naver.com>
2025-08-08 11:02:15 +0900

    [feat] CharacterManager 싱글톤 설정

--- Assets/01.Scripts/CharacterManager.cs
+++ Assets/01.Scripts/CharacterManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static CharacterManager _instance;
+    public static CharacterManager Instance
+    { get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("CharacterManager").AddComponent<CharacterManager>();
+            }
+            return _instance;
+        }
+    }    
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
