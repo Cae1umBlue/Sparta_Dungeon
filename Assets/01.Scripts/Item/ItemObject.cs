17ce8fdd206ae6535accc6eef0fc26eab7533eea
SeungYeop Oh <sadrain9274@naver.com>
2025-08-08 18:04:46 +0900

    [feat] IInteractable 인터페이스 상속

--- Assets/01.Scripts/Item/ItemObject.cs
+++ Assets/01.Scripts/Item/ItemObject.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemObject : MonoBehaviour
+
+public interface IInteractable
 {
-    // Start is called before the first frame update
-    void Start()
+    public string GetInteractPrompot(); // UI에 표시할 정보
+    public void OnInteract(); // 상호작용 호출
+}
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData data;
+
+    public string GetInteractPrompot()
     {
-        
+        throw new System.NotImplementedException();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnInteract()
     {
-        
+        throw new System.NotImplementedException();
     }
 }
