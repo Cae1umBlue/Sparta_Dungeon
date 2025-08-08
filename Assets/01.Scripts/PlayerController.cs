07822520cc5c65a48fe6febdbd5ea09089221bcf
SeungYeop Oh <sadrain9274@naver.com>
2025-08-08 11:19:07 +0900

    [feat] PlayerController 필드, 변수 선언

--- Assets/01.Scripts/PlayerController.cs
+++ Assets/01.Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpPower;
+    public LayerMask groundLayerMask;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("Look")]
+    public Transform cameraContainer;
+    public float mixXLook;
+    public float maxXLook;
+    private float camCurXRot;
+    public float lookSensitivity;
+    private Vector2 mouseDelta;
+
+    [HideInInspector]
+    public bool canLook = true;
+
+    private Rigidbody rb;
+
 }
