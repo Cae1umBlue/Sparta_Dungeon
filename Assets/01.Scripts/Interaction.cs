2dd4fd8f47b9059398867670596ee08de76e8e0d
SeungYeop Oh <sadrain9274@naver.com>
2025-08-11 09:30:04 +0900

    [feat] Interaction 스크립트 추가 및 필드 설정

---
+++ Assets/01.Scripts/Interaction.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    public float checkRate = 0.05f; // 아이템 체크에 필요한 시간
+    private float lastCheckTime; // 마지막 체크에 걸린 시간
+    public float maxCheckDistance; // 최대 체크 거리
+    public LayerMask layerMask;
+
+    public GameObject curInteractionObject; // 현재 상호작용 아이템 오브젝트
+    public IInteractable curInteractable; // 현재 상호작용 인터페이스
+}
