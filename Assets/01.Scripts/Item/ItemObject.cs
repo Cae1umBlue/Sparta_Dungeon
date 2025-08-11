27e9ace23a9b151b0c0c5d14a33d24323e7e388a
SeungYeop Oh <sadrain9274@naver.com>
2025-08-11 09:19:46 +0900

    [feat] UI 아이템 정보 표시

--- Assets/01.Scripts/Item/ItemObject.cs
+++ Assets/01.Scripts/Item/ItemObject.cs
@@ -12,9 +12,10 @@ public class ItemObject : MonoBehaviour, IInteractable
 {
     public ItemData data;
 
-    public string GetInteractPrompot()
+    public string GetInteractPrompot() // UI에 표시할 정보 (아이템 이름, 설명)
     {
-        throw new System.NotImplementedException();
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
     }
 
     public void OnInteract()
