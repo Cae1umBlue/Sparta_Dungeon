73fab43c32b8840696038523f38488d573a347bb
SeungYeop Oh <sadrain9274@naver.com>
2025-08-11 14:41:51 +0900

    [feat] ItemSlot 스크립트 추가(아이템 정보 ON/OFF)

--- Assets/01.Scripts/Item/ItemSlot.cs
+++ Assets/01.Scripts/Item/ItemSlot.cs
@@ -14,5 +14,40 @@ public class ItemSlot : MonoBehaviour
     private Outline outline;
 
     public int index;
+    public bool equipped;
     public int quantity;
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    private void OnEnable()
+    {
+        outline.enabled = equipped;
+    }
+
+    public void Set()
+    {
+        Icon.gameObject.SetActive(true);
+        Icon.sprite = item.icon;
+        quatityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
+
+        if (outline != null)
+        {
+            outline.enabled = equipped;
+        }
+    }
+
+    public void Clear()
+    {
+        item = null;
+        Icon.gameObject.SetActive(false);
+        quatityText.text = string.Empty;
+    }
+
+    public void OnClickButton()
+    {
+        inventory.SelectItem(index);
+    }
 }
